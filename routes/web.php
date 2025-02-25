ecdac128af8cc281c86632f42be9917a49ce3567
Huỳnh Thị Tố Loan <loan.huynh25@student.passerellesnumeriques.org>
2025-02-25 11:44:31 +0700

    Bài tập run file index.html

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\PageController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/baitap4', [PageController::class, 'index']);
